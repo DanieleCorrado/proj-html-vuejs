986f36a222364b5655ebc8dee0258d783073c9fb
DanieleCorrado <corrado_daniele@virgilio.it>
2023-06-13 11:50:02 +0200

    add about networ section to main

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -71,6 +71,79 @@ export default {
 
   </section>
 
+  <!-- Sezione about network -->
+
+  <section id="about-network">
+    
+    <div id="network-description">
+
+      <h5>ABOUT THE NETWORK</h5>
+
+      <div id="name">
+        <div id="background">
+        <h2>The</h2>
+        </div>
+          <H2>Company</H2>
+      </div>
+
+      <!-- desrizione azienda -->
+
+      <p>With all of this expertise and capability comes an unrivalled commitment to customer service.</p>
+
+      <!-- Capacità aziendali -->
+
+      <ul>
+        <li>
+
+          <!-- Visualizzazione percentuale -->
+          
+          <div class="radialProgressBar">
+            <div class="overlay"><span class="prc-number">0</span>%</div>
+          </div>
+          
+          <div class="information">
+            <h2>Leadership</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+
+        </li>
+
+        <li>
+
+          <!-- Visualizzazione percentuale -->
+
+          <div class="radialProgressBar">
+            <div class="overlay"><span class="prc-number">0</span>%</div>
+          </div>
+          
+          <div class="information">
+            <h2>Psychology</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+
+        </li>
+
+        <li>
+
+          <!-- Visualizzazione percentuale -->
+
+          <div class="radialProgressBar progress-1">
+            <div class="overlay"><span class="prc-number">1</span>%</div>
+          </div>
+          
+          <div class="information">
+            <h2>Flexibility</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+
+        </li>
+
+      </ul>
+
+    </div>     
+
+  </section>
+
 </main>
 
 </template>
@@ -93,7 +166,7 @@ main {
       width: 80%;
       margin: 0 auto;
 
-      // Regole generali dei componenti della sezione
+      // Regole comuni dei componenti della sezione
 
       #general-description, #technology-description, #transport-description {
         margin: 60px 0;
@@ -107,6 +180,8 @@ main {
         h5 {
           color: $primary;
         }
+
+        // Regole comuni dei componenti della sezione
 
         h5, h2, p, #tags {
           margin-bottom: 30px;
@@ -150,7 +225,92 @@ main {
     }
   }
 
-}
+  // Regole sezione about network
 
+  #about-network {
+    background-color: black;
+    background-image: url(../assets/img/about-5.jpg);
+    background-repeat: no-repeat;
+    background-position: right;
+    background-size: contain;
+    padding: 20px 0;
+
+    // Regole descrizione network
+
+    #network-description {
+      width: 50%;
+      margin: 0 40px;
+      color: white;
+
+      // Regole comuni dei componenti della sezione
+
+      h5, #name, p {
+        margin-bottom: 30px;
+      }
+
+      h5 {
+        color: $primary;
+      }
+
+      #name {
+          display: flex;
+
+          #background {
+            display: inline-block;
+            background-color: rgba(4, 131, 131, 0.4);
+            padding: 0 5px;
+
+            h2 {
+              color: $primary;
+            }
+          }
+        }
+
+        ul {
+          li {
+            display: flex;
+            align-items: center;
+            
+            // Regole cerchio percentuale
+            .radialProgressBar {
+              border-radius: 50%;
+              width: 100px;
+              height: 100px;
+              display: flex;
+              background: rgb(35, 35, 40);
+              margin: 20px;
+            }
+
+            .radialProgressBar .overlay {
+              border-radius: 50%;
+              width: 80px;
+              height: 80px;
+              margin: auto;
+              background: black;
+              text-align: center;
+              padding-top: 25%;
+            }
+
+            .overlay {
+
+              .prc-number {
+                font-size: 30px;
+              }
+            }
+            .progress-1 {
+              background-image: linear-gradient(0deg, #232328 50%, transparent 50%), linear-gradient(3.6deg, $primary 50%, #232328 50%);
+            }
+
+            // Regole informazioni
+            .information {
+              margin-top: 20px;
+            }
+          }
+        }
+
+    }
+  }
+
+}
 
 </style>
