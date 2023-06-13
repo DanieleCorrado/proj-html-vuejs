ce4d4f27eee9191dd141a962718246e29fe2e955
DanieleCorrado <corrado_daniele@virgilio.it>
2023-06-13 19:03:13 +0200

    add get in touch section to main

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,5 +1,7 @@
 <script>
 
+import contacts from '../dataset/contacts.json';
+
 export default {
   name: "PageMain",
 
@@ -7,10 +9,18 @@ export default {
     return {
       userName: '',
       userEmail: '',
-      subscribers: []
+      userPhone: '',
+      userOtherInfo: '',
+      userMessage: '',
+      subscribers: [],
+      messages: [],
+      contact: contacts
     }
   },
   methods: {
+
+    // Aggiunge indirizzo email e nome dell'utente all'elenco di iscritti alla newsletter
+
     subscribe(e) {
 
       e.preventDefault();
@@ -20,8 +30,25 @@ export default {
       })
       this.userEmail = '';
       this.userName = ''
-      console.log(this.subscribers)
+    },
 
+    // Aggiunge indirizzo email,nome utente, numero di telefono, altre informazioni e il messaggio all'elenco di messaggi inviati dagli utenti
+
+    sendMessage(e) {
+      e.preventDefault();
+      this.messages.push({
+        name: this.userName,
+        email: this.userEmail,
+        phone: this.userPhone,
+        otherInfos: this.userOtherInfo,
+        message: this.userMessage
+      })
+
+      this.userName = '';
+      this.userEmail = '';
+      this.userPhone = '';
+      this.userOtherInfo = '';
+      this.userMessage = '';
     }
   },
 }
@@ -96,6 +123,8 @@ export default {
     <!-- Sezione about network -->
 
     <section id="about-network">
+
+      <!-- Descrizione sezione -->
 
       <div id="network-description">
 
@@ -193,6 +222,7 @@ export default {
       <div class="app-logo">
         <img src="../assets/img/logo-5.png" alt="Logo LESS">
       </div>
+
     </section>
 
     <!-- Sezione news -->
@@ -294,10 +324,115 @@ export default {
 
       <div id="newsletter-form">
         <form id="subscription" v-on:submit="subscribe">
-          <input type="text" id="name" placeholder="Name" v-model="userName">
-          <input type="email" id="name" placeholder="Email" v-model="userEmail">
+          <input type="text" id="name" placeholder="Name" v-model="userName" required>
+          <input type="email" id="email" placeholder="Email" v-model="userEmail" required>
           <button type="submit">SUBSCRIBE</button>
         </form>
+      </div>
+
+    </section>
+
+    <!-- Sezione get in touch -->
+
+    <section id="get-in-touch">
+
+      <div id="send-message">
+
+        <!-- Descrizione sezione -->
+
+        <div id="message-description">
+          <h5>SEND A MESSAGE</h5>
+
+          <div id="name">
+            <H2>Get in</H2>
+            <div id="background">
+              <h2>Touch</h2>
+            </div>
+          </div>
+
+          <p>We will respond to your message as soon as possible</p>
+
+        </div>
+
+        <!-- Modulo invio messaggio -->
+
+        <div id="message-form">
+
+          <form id="send-message" v-on:submit="sendMessage">
+
+            <div id="first-row">
+
+              <input type="text" id="message-name" placeholder="Name" v-model="userName" required>
+              <input type="email" id="message-email" placeholder="Email" v-model="userEmail" required>
+
+            </div>
+
+            <div id="second-row">
+
+              <input type="text" id="message-number" placeholder="Phone" v-model="userPhone" required>
+              <select id="other-info" v-model="userOtherInfo">
+                <option value="altre">altre</option>
+                <option value="opzioni">opzioni</option>
+              </select>
+
+            </div>
+
+            <div id="third-row">
+              <textarea type="text" id="message-text" placeholder="Message" v-model="userMessage" required></textarea>
+            </div>
+
+            <button type="submit">SEND</button>
+
+          </form>
+
+        </div>
+
+      </div>
+
+      <!-- Contatti azienda -->
+
+      <div id="contact">
+
+        <h2>Exemple Inc.</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Sint corporis magni ad nobis optio rerum quidem obcaecati temporibus, iusto qui quo autem labore, esse
+          consequuntur quibusdam amet accusantium.
+        </p>
+
+        <!-- Lista contatti azienda -->
+
+        <ul>
+
+          <li>
+            <div class="circle">
+              <font-awesome-icon icon="fa-solid fa-phone" />
+            </div>
+            <span id="phone">
+              +{{ contact[0].stateCode }}
+              <span v-if="contact[0].areaCode">({{ contact[0].areaCode }})</span>
+              {{ contact[0].phoneNumber }}
+            </span>
+          </li>
+
+          <li>
+            <div class="circle">
+              <font-awesome-icon icon="fa-solid fa-envelope" />
+            </div>
+            <span id="mail"> {{ contact[1].mail }} </span>
+          </li>
+
+          <li>
+            <div class="circle">
+              <font-awesome-icon icon="fa-solid fa-location-dot" />
+            </div>
+            <span id="street">{{ contact[2].streetName }}, {{ contact[2].houseNumber }}</span>
+          </li>
+
+        </ul>
+
       </div>
 
     </section>
@@ -325,6 +460,18 @@ main {
         color: $primary;
       }
     }
+  }
+
+  // Regole input e select form
+
+  input,
+  select {
+    width: 300px;
+    padding: 10px;
+    filter: opacity(0.7);
+    margin-bottom: 20px;
+    border-radius: 5px;
+    border: none;
   }
 
   // Regole sezione logistica
@@ -484,14 +631,18 @@ main {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 80%;
+    width: 90%;
     margin: 0 auto;
 
     // Regole logo
     .app-logo {
       padding: 60px 0;
-      width: calc(100% / 6);
+      width: calc(100% / 6 - 20px);
       text-align: center;
+
+      img {
+        width: 80%;
+      }
     }
   }
 
@@ -583,8 +734,7 @@ main {
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-    padding-top: 50px;
-    height: 400px;
+    padding: 50px 0;
     background-image: url(../assets/img/bg-1.jpg);
     background-position: center;
     background-size: cover;
@@ -617,23 +767,121 @@ main {
       }
     }
 
-    // Regole form newsletter
+  }
 
-    #newsletter-form {
+  // Regole sezione get in touch
 
-      #subscription {
-        input {
-          width: 300px;
-          padding: 10px;
-          filter: opacity(0.7);
+  #get-in-touch {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-evenly;
+    padding: 50px 0;
+
+    // Regole comuni dei componenti della sezione
+
+    #send-message,
+    #contact {
+      width: calc(100% / 2 - 20px);
+    }
+
+    // Regole invio messaggio
+
+    #send-message {
+
+      // Regole descrizione sezione
+
+      #message-description {
+
+        h5,
+        #name,
+        p {
           margin-bottom: 20px;
-          border-radius: 5px;
-          border: none;
+        }
+
+        h5 {
+          color: $primary;
+        }
+      }
+
+      // Regole form messaggio
+
+      #message-form {
+
+        input,
+        select {
+          width: calc(100% / 2 - 20px)
+        }
+
+        #send-message {
+          width: 100%;
+
+          #first-row {
+            width: 100%;
+
+            #message-email {
+              margin-left: 20px;
+            }
+          }
+
+          #second-row {
+
+            #other-info {
+              margin-left: 20px;
+            }
+          }
+
+          #third-row {
+            #message-text {
+              width: calc(100% - 20px);
+              height: 200px;
+              margin-bottom: 20px;
+              background-color: rgb(250, 252, 249);
+            }
+          }
+        }
+      }
+    }
+
+    // Regole contact
+
+    #contact {
+      width: 20%;
+
+      h2,
+      p {
+        margin-bottom: 30px;
+      }
+
+      // Regole lsita contatti azienda
+
+      ul {
+        li {
+          margin-bottom: 20px;
+          color: $primary;
+          display: flex;
+          align-items: center;
+
+          .circle {
+            background-color: rgba(4, 131, 131, 0.4);
+            border-radius: 50%;
+            padding: 15px;
+
+            .fa-location-dot {
+              font-size: 14px;
+              padding: 0 3px;
+            }
+          }
+
+          #phone,
+          #mail,
+          #street {
+            margin-left: 20px;
+          }
         }
       }
     }
 
   }
-
 }
 </style>
